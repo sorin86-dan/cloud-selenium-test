66b225de9fd97fd4a1931792214d3471209a515a
Sorin Dan <dansorin2006@gmail.com>
2022-06-15 21:36:42 +0300

    fix(master): Updated tests according to updated website

--- src/test/java/com/testing/BlogPageTest.java
+++ src/test/java/com/testing/BlogPageTest.java
@@ -19,10 +19,10 @@ public class BlogPageTest extends BaseTest {
         BlogPage blogPage = new BlogPage(webDriver).clickBlogMenu();
 
         assertEquals(blogPage.getPageTitle(), "Blog");
-        assertEquals(blogPage.getArticleTitle(1),"Containerization in test automation (III) – UI testing using Selenium Grid and Testcontainers");
-        assertEquals(blogPage.getArticleTitle(2),"Test automation and Cloud technologies — Part II: Using AWS ECR and AWS EC2");
-        assertEquals(blogPage.getArticleTitle(3),"Containerization in test automation (II) – Containerization with microservices testing");
-        assertEquals(blogPage.getArticleTitle(4),"Containerization in test automation (I) – Containerization with UI testing");
+        assertEquals(blogPage.getArticleTitle(1),"Test automation and Containerization (IV) – UI testing using Selenium Grid and Kubernetes");
+        assertEquals(blogPage.getArticleTitle(2),"Test automation and Containerization (III) – UI testing using Selenium Grid and Testcontainers");
+        assertEquals(blogPage.getArticleTitle(3),"Test automation and Cloud technologies — Part II: Using AWS ECR and AWS EC2");
+        assertEquals(blogPage.getArticleTitle(4),"Test automation and Containerization (II) – Containerization with microservices testing");
     }
 
 }
